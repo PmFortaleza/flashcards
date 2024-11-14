6d1deb71e6f99d57f083abc50ca9efcdbb971dbf
PmFortaleza <141734491+PmFortaleza@users.noreply.github.com>
2024-11-14 07:32:09 -0300

    14/11/2024

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'GEOGRAFIA',
+    'Qual o rio mais longo do mundo?',
+    'Rio Nilo '
+)
+criaCartao(
+    'GEOGRAFIA',
+    'Em qual continente está localizado o deserto do Saara?',
+    'Esta localizado no continente Africano',
+)
+criaCartao (
+    'GEOGRAFIA',
+    'Qual a capital da Colombia',
+    'Bogotá'
+)
